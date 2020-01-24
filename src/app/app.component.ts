b295b8f80b91fdeb5922e8813d8d6e4c47af54e0
Nelson W Foltz <nelsonwfoltz@Nelsons-iMac.fios-router.home>
2020-01-24 11:03:45 -0500

    added property for entered text and compare against random sentence to conditionally display success message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { lorem } from 'faker';
 
-lorem.sentence();
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = lorem.sentence();
+  enteredText = '';
+
+  onInput(value: string) {
+    this.enteredText = value;
+  }
 }
